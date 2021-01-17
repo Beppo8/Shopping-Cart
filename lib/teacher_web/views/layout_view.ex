709c670e4e9e23f6479613cf2ebb9a741749304f
beppo <beppo@localhost.localdomain>
2021-01-17 13:20:46 -0600

    added: layout_view

--- lib/teacher_web/views/layout_view.ex
+++ lib/teacher_web/views/layout_view.ex
@@ -1,3 +1,14 @@
 defmodule TeacherWeb.LayoutView do
   use TeacherWeb, :view
+
+  alias Teacher.Workers.CartAgent
+
+  def cart_item_count(current_user) do
+    case CartAgent.get_cart(current_user.username) do
+      nil ->
+        0
+      cart ->
+        Enum.count(cart)
+    end
+  end
 end
