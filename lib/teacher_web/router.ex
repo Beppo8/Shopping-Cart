a7543d8f1b265739e41c767301ad338908c57a81
beppo <beppo@localhost.localdomain>
2021-01-17 13:06:42 -0600

    codebase

---
+++ lib/teacher_web/router.ex
@@ -0,0 +1,30 @@
+defmodule TeacherWeb.Router do
+  use TeacherWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+    plug TeacherWeb.Plugs.SetAuth
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", TeacherWeb do
+    pipe_through :browser
+
+    resources "/sessions", SessionController, only: [:new, :create]
+    delete "/sign-out", SessionController, :delete
+    resources "/registrations", RegistrationController, only: [:new, :create]
+
+    resource "/cart", CartController, only: [:update]
+
+    resources "/albums", AlbumController
+    resources "/users", UserController, only: [:show]
+    get "/", PageController, :index
+  end
+end
