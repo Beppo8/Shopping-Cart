335c618d3a7328e6e4e3524ec7e4ad7fc31e5064
beppo <beppo@localhost.localdomain>
2021-01-17 21:32:57 -0600

    delete: cart_controller.ex

--- lib/teacher_web/controllers/cart_controller.ex
+++ lib/teacher_web/controllers/cart_controller.ex
@@ -12,4 +12,13 @@ defmodule TeacherWeb.CartController do
     |> put_flash(:info, "Album added to your cart")
     |> redirect(to: Routes.album_path(conn, :show, album))
   end
+
+  def delete(%{assigns: %{current_user: current_user}} = conn, %{"id" => id}) do
+    album = Recordings.get_album!(id)
+    CartAgent.delete_item(current_user.username, album.id)
+
+    conn
+    |> put_flash(:info, "Album removed from your cart")
+    |> redirect(to: Routes.album_path(conn, :show, album))
+  end
 end
