d01901403c6cbd41445754c536850e244b7641ad
beppo <beppo@localhost.localdomain>
2021-01-17 21:40:17 -0600

    added: album_view

--- lib/teacher_web/views/album_view.ex
+++ lib/teacher_web/views/album_view.ex
@@ -2,4 +2,29 @@ defmodule TeacherWeb.AlbumView do
   use TeacherWeb, :view
   import Scrivener.HTML
 
+  alias Teacher.Workers.CartAgent
+
+  def cart_link(conn, current_user, album) do
+    if in_cart?(cirrent_user.username, album.id) do
+      link "Remove from cart", to: Routes.cart_path(conn, :delete, album.id), method: :delete
+    else
+      link "Add to cart", to: Routes.cart_path(conn, :update, album.id), method: :put
+    end
+  end
+
+  defp in_cart?(username, album_id) do
+    username
+    |> existing_ids()
+    |> Enum.member?(album_id)
+  end
+
+  defp existing_ids(username) do
+    case CartAgent.get_cart(username) do
+      nil ->
+        []
+      cart ->
+        Enum.map(cart, &(&1.id))
+    end
+  end
+
 end
